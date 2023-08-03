eab079342107d16999886f8b3fb2db2fafb100a3
devzero-inc <kevin@devzero.io>
2023-08-03 23:24:08 +0000

    version 0.148.0 of dz-cli

--- Formula/dz-cli@0.148.0.rb
+++ Formula/dz-cli@0.148.0.rb
@@ -1,4 +1,4 @@
-class DzCli < Formula
+class DzCliAT01480 < Formula
     desc "CLI to create and use ephemeral dev environments on devzero!"
     homepage "https://www.devzero.io/"
     version "0.148.0"
