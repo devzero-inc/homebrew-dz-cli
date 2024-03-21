db2c8c072e6ba20616ff33066f77a5f7ebb838f4
cmiller01 <cmiller01@users.noreply.github.com>
2024-03-21 20:39:40 +0000

    Release dz-cli 8ceee39

--- Formula/dz-cli.rb
+++ Formula/dz-cli.rb
@@ -1,21 +1,21 @@
 class DzCli < Formula
   desc "Command-line tool for DevZero"
   homepage "https://www.devzero.io/"
-  version "8c50091"
-  version_scheme 19
+  version "8ceee39"
+  version_scheme 20
 
   on_macos do
     on_arm do
-      url "https://dz-cli.s3.us-west-2.amazonaws.com/8c50091/darwin-arm64/dz.tar.gz"
-      sha256 "027947ea455b2f74a0b8368dff218515e9c3be09674f219f66ef06582503bb06"
+      url "https://dz-cli.s3.us-west-2.amazonaws.com/8ceee39/darwin-arm64/dz.tar.gz"
+      sha256 "88e8d8beffb3b74fa81c9b7d06d4628c4d703103e0de2686a9d5541204d83f59"
       def install
         bin.install "dz"
       end
     end
 
     on_intel do
-      url "https://dz-cli.s3.us-west-2.amazonaws.com/8c50091/darwin-amd64/dz.tar.gz"
-      sha256 "ebaf5117457df6b1d9b96319b39ae80d05831f46fce8e25508d71b57a6fe787b"
+      url "https://dz-cli.s3.us-west-2.amazonaws.com/8ceee39/darwin-amd64/dz.tar.gz"
+      sha256 "95b24aec01c1f13d02c077ea684ca34eb6a56e30686c11e34db6819ecc9c2a46"
       def install
         bin.install "dz"
       end
@@ -24,16 +24,16 @@ class DzCli < Formula
 
   on_linux do
     on_arm do
-      url "https://dz-cli.s3.us-west-2.amazonaws.com/8c50091/linux-arm64/dz.tar.gz"
-      sha256 "fbf793a4ce369f1875ed5fbd7841f50622d0811b73360df8c8270d45d2b26a93"
+      url "https://dz-cli.s3.us-west-2.amazonaws.com/8ceee39/linux-arm64/dz.tar.gz"
+      sha256 "f08f7ab918a4c45151ed977e5adeb7960cef164c2d8e67c42f851a38e5b10019"
       def install
         bin.install "dz"
       end
     end
 
     on_intel do
-      url "https://dz-cli.s3.us-west-2.amazonaws.com/8c50091/linux-amd64/dz.tar.gz"
-      sha256 "888cafc1f381e2893ef116d457ee9ecdab3dc609048cb775995b7bf6a4ae0dac"
+      url "https://dz-cli.s3.us-west-2.amazonaws.com/8ceee39/linux-amd64/dz.tar.gz"
+      sha256 "ca9246ba9ee58f3c51f8d69c4d85a4920e1502eec1540838b748bf21e9529080"
       def install
         bin.install "dz"
       end
