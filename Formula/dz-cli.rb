234bb56dc2292adf785c430eb6e7d53e3a54939d
stutibiyani <stutibiyani@users.noreply.github.com>
2024-05-06 17:51:34 +0000

    Release dz-cli 3cfbf68

--- Formula/dz-cli.rb
+++ Formula/dz-cli.rb
@@ -1,21 +1,21 @@
 class DzCli < Formula
   desc "Command-line tool for DevZero"
   homepage "https://www.devzero.io/"
-  version "1d40e70"
-  version_scheme 32
+  version "3cfbf68"
+  version_scheme 33
 
   on_macos do
     on_arm do
-      url "https://dz-cli.s3.us-west-2.amazonaws.com/1d40e70/darwin-arm64/dz.tar.gz"
-      sha256 "ab6530ea8e27c4643e8bc2b4300cf018fef98d019da1f5def1993903a668d7dd"
+      url "https://dz-cli.s3.us-west-2.amazonaws.com/3cfbf68/darwin-arm64/dz.tar.gz"
+      sha256 "1fe3ed23ae778e251df6d9beb42bc861aa2366a7597f11dd723ee9886a4c421f"
       def install
         bin.install "dz"
       end
     end
 
     on_intel do
-      url "https://dz-cli.s3.us-west-2.amazonaws.com/1d40e70/darwin-amd64/dz.tar.gz"
-      sha256 "4af1f1f068ccdee4bd0a51bf7600f0796f55c392ecf60930669db9bc5f8e386a"
+      url "https://dz-cli.s3.us-west-2.amazonaws.com/3cfbf68/darwin-amd64/dz.tar.gz"
+      sha256 "549ef6d387f61bce0bd76650bcb23d1a2dce7a6775222573a914461f3de1ca0b"
       def install
         bin.install "dz"
       end
@@ -24,16 +24,16 @@ class DzCli < Formula
 
   on_linux do
     on_arm do
-      url "https://dz-cli.s3.us-west-2.amazonaws.com/1d40e70/linux-arm64/dz.tar.gz"
-      sha256 "63cc7800c5ebe9da6c5b2f67c59073883ff9f9175f4f6111dfbeb348ccccac2d"
+      url "https://dz-cli.s3.us-west-2.amazonaws.com/3cfbf68/linux-arm64/dz.tar.gz"
+      sha256 "45359b56c02609f63ed874aa9876831f3e3bddf7c57d8c9fe594861ff36bfff3"
       def install
         bin.install "dz"
       end
     end
 
     on_intel do
-      url "https://dz-cli.s3.us-west-2.amazonaws.com/1d40e70/linux-amd64/dz.tar.gz"
-      sha256 "180c2b518d83e72c6bfd82aeccc9e27aa4be369a9e6363988c90a45d7910cd89"
+      url "https://dz-cli.s3.us-west-2.amazonaws.com/3cfbf68/linux-amd64/dz.tar.gz"
+      sha256 "efac5bbaaf880039e916d025423d22b62d3231bb8dcd80f9ca43427a261316ca"
       def install
         bin.install "dz"
       end
