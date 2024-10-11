e2ee4a3a9e460504b92f7f12b2af1340f353c5c7
kevin-devzero <kevin-devzero@users.noreply.github.com>
2024-10-11 17:55:48 +0000

    Release dz-cli 2e8a33f

--- Formula/dz-cli.rb
+++ Formula/dz-cli.rb
@@ -1,21 +1,21 @@
 class DzCli < Formula
   desc "Command-line tool for DevZero"
   homepage "https://www.devzero.io/"
-  version "80951e4"
-  version_scheme 39
+  version "2e8a33f"
+  version_scheme 40
 
   on_macos do
     on_arm do
-      url "https://dz-cli.s3.us-west-2.amazonaws.com/80951e4/darwin-arm64/dz.tar.gz"
-      sha256 "444e234c4bdabbd1208e7bb0389b2f1ab5854b9af1dd2cd750448ee5b74a8d8b"
+      url "https://dz-cli.s3.us-west-2.amazonaws.com/2e8a33f/darwin-arm64/dz.tar.gz"
+      sha256 "28e200d6d76e84071b41f067faca94aae4d4347bae696807141771bbab8e42a0"
       def install
         bin.install "dz"
       end
     end
 
     on_intel do
-      url "https://dz-cli.s3.us-west-2.amazonaws.com/80951e4/darwin-amd64/dz.tar.gz"
-      sha256 "ada6764f2c9f4bbade24a5308038f45d27ba0ff3a2a6555c997def50545ebc93"
+      url "https://dz-cli.s3.us-west-2.amazonaws.com/2e8a33f/darwin-amd64/dz.tar.gz"
+      sha256 "35b7a890f754fd8765ccde73251e1ac6bac338026d6ee2ee5ad824ddcf720dec"
       def install
         bin.install "dz"
       end
@@ -24,16 +24,16 @@ class DzCli < Formula
 
   on_linux do
     on_arm do
-      url "https://dz-cli.s3.us-west-2.amazonaws.com/80951e4/linux-arm64/dz.tar.gz"
-      sha256 "2e53eedb335872e0653db5ec61dbb3df6052b709809a790aa6aa713e5c26740b"
+      url "https://dz-cli.s3.us-west-2.amazonaws.com/2e8a33f/linux-arm64/dz.tar.gz"
+      sha256 "82d6787590e1e4bb55dbe135dbf05f41c04a19a091bc9b59d1e802424e46903e"
       def install
         bin.install "dz"
       end
     end
 
     on_intel do
-      url "https://dz-cli.s3.us-west-2.amazonaws.com/80951e4/linux-amd64/dz.tar.gz"
-      sha256 "def905cf17a794d7b209ca77ccd5546111617ad7f1f3429d71f11083dfccf924"
+      url "https://dz-cli.s3.us-west-2.amazonaws.com/2e8a33f/linux-amd64/dz.tar.gz"
+      sha256 "82b4d6be2256f66decb8b1a64c751c6f35ef9d29145435570058130586764398"
       def install
         bin.install "dz"
       end
